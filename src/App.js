0391f231af45c518df1dd5a8f960fe1ca5a58195
Ankita <ankum1999@gmail.com>
2023-07-05 11:56:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import Dice from "./components/Dice";
 import Confetti from "react-confetti";
 
 export default function App() {
-  /**
-   * Challenge: Add conditional styling to the Die component
-   * so that if it's held (isHeld === true), its background color
-   * changes to a light green (#59E391)
-   *
-   * Remember: currently the Die component has no way of knowing
-   * if it's "held" or not.
-   */
 
   const [dice, setDice] = useState(allNewDice());
   const [tenzies, setTenzies] = useState(false);
